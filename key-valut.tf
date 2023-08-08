151404b3c7fffdbb9011618ad940273a9a0f3c6c
manukyanv07 <manukyanv07@gmail.com>
2023-08-08 16:57:54 -0300

    Update key-valut.tf

--- key-valut.tf
+++ key-valut.tf
@@ -9,13 +9,31 @@ resource "azurerm_key_vault" "eats_website_key_vault" {
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
-  sku_name                  = "standard"
+  sku_name = "standard"
 }
 
 resource "azurerm_key_vault_access_policy" "default_policy" {
   key_vault_id = azurerm_key_vault.eats_website_key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  key_permissions         = var.kv-key-permissions-full
+  secret_permissions      = var.kv-secret-permissions-full
+  certificate_permissions = var.kv-certificate-permissions-full
+  storage_permissions     = var.kv-storage-permissions-full
+}
+
+data "azuread_service_principal" "terraform_service_principal" {
+  display_name = "github-actions"
+}
+resource "azurerm_key_vault_access_policy" "terraform_access_policy" {
+  key_vault_id = azurerm_key_vault.eats_website_key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.terraform_service_principal.object_id
 
   lifecycle {
     create_before_destroy = true
